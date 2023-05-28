5f3ae9451dd0dc99016eb81c621b437ae1c8898d
justineeyx <122433748+justineeyx@users.noreply.github.com>
2023-05-28 16:45:37 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "./components/Header"
+import Login from "./components/Login"
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Login />
     </div>
   );
 }
